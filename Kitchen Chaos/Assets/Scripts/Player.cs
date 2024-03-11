d8e0e67f034ba9e22c4627d4368d1609ed323fd8
hellooomoerta <hilding_1989@hotmail.com>
2024-03-11 11:09:37 +0100

    Animations

--- Kitchen Chaos/Assets/Scripts/Player.cs
+++ Kitchen Chaos/Assets/Scripts/Player.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 0f;
+    [SerializeField] float rotateSpeed = 10f;
 
+    private bool isWalking;
 
-    private void Start()
+    void Start()
     {
         
     }
 
-    private void Update()
+    void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDirection != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
